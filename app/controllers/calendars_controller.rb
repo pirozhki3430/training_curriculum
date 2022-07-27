639c8fe0fd7e398a2ad692b58ade2a0329e828c6
pirozhki3430 <pirozhki3430@gmail.com>
2022-07-27 16:36:59 +0900

    calendars_controller.rb内の不要なコメントアウトの削除

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,14 +1,12 @@
 class CalendarsController < ApplicationController
 
-  # １週間のカレンダーと予定が表示されるページ
+
   def index
     getWeek
     @plan = Plan.new
   end
 
-  # 予定の保存
   def create
-    # binding.pry
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -16,24 +14,19 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    # params.require(:calendars).permit(:date, :plan)
-    # params.permit(:date, :plan)
     params.require(:plan).permit(:date, :plan)
   end
 
   def getWeek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
-    # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
-    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
     @week_days = []
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
     7.times do |x| 
-      # |x|には何回繰り返したかが入る。
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
